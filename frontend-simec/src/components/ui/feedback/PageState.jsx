28257aec75ff3f62b35337ceeedd0dd1c05f2819
CampanuciJoao <dekay022@gmail.com>
2026-04-15 14:21:11 -0400

    implementando aprimoramento do simec frontend refaturado correcao de bug

--- frontend-simec/src/components/ui/feedback/PageState.jsx
+++ frontend-simec/src/components/ui/feedback/PageState.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import EmptyState from './EmptyState';
+import { EmptyState } from '@/components/ui/layout';
 import LoadingState from './LoadingState';
 
 function PageState({
