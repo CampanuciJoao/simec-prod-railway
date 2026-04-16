7fa58552633ad876d8061a08f1ab187fc77ba0d1
CampanuciJoao <dekay022@gmail.com>
2026-04-16 01:15:41 -0400

    implementando aprimoramento do simec frontend refaturado correcao de bug equipamentoForm

--- frontend-simec/src/components/ui/index.js
+++ frontend-simec/src/components/ui/index.js
@@ -1,15 +1,8 @@
+export { default as EmptyState } from './layout/EmptyState';
+
+export { default as LoadingState } from './feedback/LoadingState';
 export { default as ModalConfirmacao } from './feedback/ModalConfirmacao';
 export { default as PageState } from './feedback/PageState';
-export { default as LoadingState } from './feedback/LoadingState';
-
-export { default as Drawer } from './overlays/Drawer';
-export { default as DrawerList } from './overlays/DrawerList';
-export { default as NotificationsPanel } from './overlays/NotificationsPanel';
-
-export { default as PageHeader } from './layout/PageHeader';
-export { default as PageLayout } from './layout/PageLayout';
-export { default as PageSection } from './layout/PageSection';
-export { default as ResponsiveGrid } from './layout/ResponsiveGrid';
 
 export { default as Button } from './primitives/Button';
 export { default as Card } from './primitives/Card';
@@ -18,3 +11,16 @@ export { default as Select } from './primitives/Select';
 export { default as DateInput } from './primitives/DateInput';
 export { default as TimeInput } from './primitives/TimeInput';
 export { default as Badge } from './primitives/Badge';
+
+export { default as ActionBar } from './layout/ActionBar';
+export { default as EntityInfoGrid } from './layout/EntityInfoGrid';
+export { default as FormActions } from './layout/FormActions';
+export { default as PageHeader } from './layout/PageHeader';
+export { default as PageLayout } from './layout/PageLayout';
+export { default as PageSection } from './layout/PageSection';
+export { default as ResponsiveGrid } from './layout/ResponsiveGrid';
+export { default as ResponsiveTabs } from './layout/ResponsiveTabs';
+
+export { default as Drawer } from './overlays/Drawer';
+export { default as DrawerList } from './overlays/DrawerList';
+export { default as NotificationsPanel } from './overlays/NotificationsPanel';
