343fe80fb51b3fcc3ac9cb7afac2491e6a809547
CampanuciJoao <dekay022@gmail.com>
2026-04-15 20:43:25 -0400

    implementando aprimoramento do simec frontend refaturado correcao de bug equipamentoForm

---
+++ frontend-simec/src/components/emails/index.js
@@ -0,0 +1,5 @@
+export { default as ConfirmDeleteEmailModal } from './ConfirmDeleteEmailModal';
+export { default as EmailForm } from './EmailForm';
+export { default as EmailFormModal } from './EmailFormModal';
+export { default as EmailsTable } from './EmailsTable';
+export { default as EmailStatusIcon } from './EmailStatusIcon';
