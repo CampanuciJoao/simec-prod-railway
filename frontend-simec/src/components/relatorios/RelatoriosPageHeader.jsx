9d48665381ebf92fc935db187c658c9a241dd77c
CampanuciJoao <dekay022@gmail.com>
2026-04-15 22:47:21 -0400

    implementando aprimoramento do simec frontend refaturado correcao de bug equipamentoForm

--- frontend-simec/src/components/relatorios/RelatoriosPageHeader.jsx
+++ frontend-simec/src/components/relatorios/RelatoriosPageHeader.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilePdf, faChartColumn } from '@fortawesome/free-solid-svg-icons';
+import { faFileLines, faFilePdf } from '@fortawesome/free-solid-svg-icons';
 
-import PageHeader from '../ui/PageHeader';
-import Button from '../ui/Button';
+import { Button, PageHeader } from '@/components/ui';
 
-function RelatoriosPageHeader({ canExport = false, onExport }) {
+function RelatoriosPageHeader({ canExport, onExport }) {
   return (
     <PageHeader
-      title="Geração de Relatórios"
-      subtitle="Monte filtros, gere resultados e exporte para PDF"
-      icon={faChartColumn}
+      title="Relatórios"
+      subtitle="Gere relatórios e exporte dados consolidados do sistema"
+      icon={faFileLines}
       actions={
-        canExport ? (
-          <Button type="button" variant="danger" onClick={onExport}>
-            <FontAwesomeIcon icon={faFilePdf} />
-            Exportar para PDF
-          </Button>
-        ) : null
+        <Button
+          type="button"
+          onClick={onExport}
+          disabled={!canExport}
+        >
+          <FontAwesomeIcon icon={faFilePdf} />
+          Exportar PDF
+        </Button>
       }
     />
   );
@@ -26,7 +27,7 @@ function RelatoriosPageHeader({ canExport = false, onExport }) {
 
 RelatoriosPageHeader.propTypes = {
   canExport: PropTypes.bool,
-  onExport: PropTypes.func,
+  onExport: PropTypes.func.isRequired,
 };
 
 export default RelatoriosPageHeader;
