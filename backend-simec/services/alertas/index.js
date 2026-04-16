e819ff158e6a20ad79244a4f18fea4f9eacfb7bc
CampanuciJoao <dekay022@gmail.com>
2026-04-16 09:15:38 -0400

    implementando aprimoramento do simec frontend refaturado correcao de bug alertas

--- backend-simec/services/alertas/index.js
+++ backend-simec/services/alertas/index.js
@@ -2,8 +2,3 @@ export * from './manutencao/index.js';
 export * from './seguro/index.js';
 export * from './contrato/index.js';
 export * from './recomendacao/index.js';
-
-export { default as gerarAlertasManutencao } from './manutencao/index.js';
-export { default as gerarAlertasSeguro } from './seguro/index.js';
-export { default as gerarAlertasContrato } from './contrato/index.js';
-export { default as gerarAlertasRecomendacao } from './recomendacao/index.js';
