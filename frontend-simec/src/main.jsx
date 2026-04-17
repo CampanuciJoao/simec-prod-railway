80f0a4bc0ac6ab37b2b5910c55f9b76b7b359dc3
CampanuciJoao <dekay022@gmail.com>
2026-04-17 00:51:25 -0400

    implementando aprimoramento do simec frontend refaturado correcao de bug visual

--- frontend-simec/src/main.jsx
+++ frontend-simec/src/main.jsx
@@ -1,22 +1,16 @@
-// Ficheiro: src/main.jsx
-// VERSÃO FINAL SÊNIOR - PONTO DE ENTRADA ÚNICO E ORGANIZADO
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-// Importação dos Provedores de Contexto
-import { AuthProvider } from './contexts/AuthContext';
-import { ToastProvider } from './contexts/ToastContext';
-import { AlertasProvider } from './contexts/AlertasContext';
+import { AuthProvider } from '@/contexts/AuthContext';
+import { ToastProvider } from '@/contexts/ToastContext';
+import { AlertasProvider } from '@/contexts/AlertasContext';
 
-// Importação do Componente Principal e Estilos Globais
-import App from './App';
-import './index.css'; // Importa o orquestrador de estilos
+import App from '@/App';
+import '@/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// A renderização raiz agora configura toda a estrutura de provedores e roteamento.
 root.render(
   <React.StrictMode>
     <BrowserRouter>
