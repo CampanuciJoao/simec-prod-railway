0d8f895a445b6fa076214b8894835f3b1d51cdc3
CampanuciJoao <dekay022@gmail.com>
2026-04-17 18:44:10 -0400

    implementando aprimoramento do simec frontend refaturado correcao de bug visual

--- frontend-simec/src/components/equipamentos/index.js
+++ frontend-simec/src/components/equipamentos/index.js
@@ -1,17 +1,13 @@
-export { default as EquipamentosList } from './EquipamentosList';
-export { default as EquipamentosKpiGrid } from './EquipamentosKpiGrid';
-export { default as EquipamentosActiveFiltersBar } from './EquipamentosActiveFiltersBar';
-export { default as StatusSelector } from './StatusSelector';
-
-export { default as EquipamentoCard } from './EquipamentoCard';
-export { default as EquipamentoCardExpanded } from './EquipamentoCardExpanded';
-
-export { default as DetalhesEquipamentoTabs } from './DetalhesEquipamentoTabs';
-export { default as DetalhesEquipamentoTabContent } from './DetalhesEquipamentoTabContent';
-
-export { default as HistoricoTimelineList } from './HistoricoTimelineList';
-
 export { default as AcessorioForm } from './AcessorioForm';
 export { default as AcessoriosList } from './AcessoriosList';
-
 export { default as AnexosList } from './AnexosList';
+export { default as DetalhesEquipamentoTabContent } from './DetalhesEquipamentoTabContent';
+export { default as DetalhesEquipamentoTabs } from './DetalhesEquipamentoTabs';
+export { default as EquipamentoCard } from './EquipamentoCard';
+export { default as EquipamentoCardExpanded } from './EquipamentoCardExpanded';
+export { default as EquipamentoForm } from './EquipamentoForm';
+export { default as EquipamentosActiveFiltersBar } from './EquipamentosActiveFiltersBar';
+export { default as EquipamentosKpiGrid } from './EquipamentosKpiGrid';
+export { default as EquipamentosList } from './EquipamentosList';
+export { default as HistoricoTimelineList } from './HistoricoTimelineList';
+export { default as StatusSelector } from './StatusSelector';
